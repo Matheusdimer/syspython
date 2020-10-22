4a4557368c927ac71ecc042bef1c4c767ced1015
Matheusdimer <matheus-delima@outlook.com>
2020-10-22 01:09:10 -0300

    Adicionada funcao pesquisar com filtros

--- src/mainWindow.py
+++ src/mainWindow.py
@@ -15,10 +15,11 @@ class mainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 background: #61AB6D; 
                 color: #fff; 
                 font: Times New Roman; 
-                font-size: 14px; 
+                font-size: 10px; 
                 font-weight: 700; 
                 border-radius: 5px; 
                 height: 30px;
+                padding: 5rem;
             }
 
             QPushButton:hover {
@@ -31,7 +32,7 @@ class mainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 QTableView {
                     background: white;
                     color: #292929;
-                    font-size: 16px;
+                    font-size: 14px;
                     font: arial;
                     font-weight: 400;
                 }
@@ -43,10 +44,11 @@ class mainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 background: #3C8FBC; 
                 color: #fff; 
                 font: Times New Roman; 
-                font-size: 14px; 
+                font-size: 10px; 
                 font-weight: 700; 
                 border-radius: 5px; 
-                height: 30px
+                height: 30px;
+                padding: 5rem;
             }
 
             QPushButton:hover {
@@ -54,8 +56,58 @@ class mainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             }
             """
         )
-        self.dados()
+        self.filter = "CODIGO"
+        self.toolFilter.setText("Filtro: Código")
+
+        codigo = QtWidgets.QAction(text="Código", parent=self.toolFilter)
+        codigo.triggered.connect(self.setFilterCod)
+        self.toolFilter.addAction(codigo)
         
+        desc = QtWidgets.QAction(text="Descrição", parent=self.toolFilter)
+        desc.triggered.connect(self.setFilterDesc)
+        self.toolFilter.addAction(desc)
+
+        tipo = QtWidgets.QAction(text="Tipo", parent=self.toolFilter)
+        tipo.triggered.connect(self.setFilterTipo)
+        self.toolFilter.addAction(tipo)
+
+        self.table.setHorizontalHeaderLabels(["Código", "Descrição", "Valor Unitário", "Tipo"])
+
+        self.btn_search.clicked.connect(self.search)
+        self.btn_refresh.clicked.connect(self.dados)
+
+        self.dados()
+    
+    def dados(self, coluna="", busca=""):
+        banco = Banco("./src/database/dados.db")
+        self.result = banco.consulta("Produtos", pesquisa=busca, campo=coluna)
+        self.collumn = len(self.result[0])
+        self.row = len(self.result)
+        self.table.setRowCount(0)
+        self.table.setColumnCount(self.collumn)
+        self.table.setHorizontalHeaderLabels(["Código", "Descrição", "Valor Unitário", "Tipo"])
+        self.table.setColumnWidth(0, 60)
+        self.table.setColumnWidth(1, 380)
+        self.table.setColumnWidth(2, 120)
+        self.table.setColumnWidth(3, 180)
+        for row_number, row_data in enumerate(self.result):
+            self.table.insertRow(row_number)
+            for collumn_number, data in enumerate(row_data):
+                
+                if collumn_number == 2:
+                    valor = (f"R$ {data:.2f}")
+                    self.item = QtWidgets.QTableWidgetItem(str(valor).replace(".", ","))
+                elif collumn_number == 0:
+                    valor = (f"{data:0>4}")
+                    self.item = QtWidgets.QTableWidgetItem(str(valor))
+                else:
+                    self.item = QtWidgets.QTableWidgetItem(str(data))
+                if collumn_number == 0 or collumn_number == 2:
+                    self.item.setTextAlignment(QtCore.Qt.AlignRight)
+                else:
+                    self.item.setTextAlignment(QtCore.Qt.AlignLeft)
+                self.table.setItem(row_number, collumn_number, self.item)
+
     def openCad(self):
         self.window = QtWidgets.QMainWindow()
         self.ui = Ui_MainWindow2()
@@ -115,6 +167,23 @@ class mainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             #self.labelinfo.setText("Erro ao cadastrar produto, verifique os campos.")
             self.msgInfo("Erro ao cadastrar produto, verifique os campos.")
 
+    def search(self):
+        self.lineBusca = self.lineSearch.text()
+        self.dados(busca=self.lineBusca, coluna=self.filter)
+
+    def setFilterTipo(self):
+        self.toolFilter.setText("Filtro: Tipo")
+        self.filter = "TIPO"
+
+    def setFilterDesc(self):
+        self.toolFilter.setText("Filtro: Descrição")
+        self.filter = "DESCRICAO"
+
+    def setFilterCod(self):
+        self.toolFilter.setText("Filtro: Código")
+        self.filter = "CODIGO"
+
+
     def msgInfo(self, text):
         self.aviso = QtWidgets.QWidget()
         self.interface = Ui_Form()
@@ -123,33 +192,3 @@ class mainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.interface.label.setAlignment(QtCore.Qt.AlignCenter)
         self.interface.label.setText(text)
         self.interface.bntExit.clicked.connect(self.aviso.close)
-
-    def dados(self):
-        banco = Banco("./src/database/dados.db")
-        self.result = banco.consulta("Produtos")
-        self.collumn = len(self.result[0])
-        self.row = len(self.result)
-        self.table.setRowCount(0)
-        self.table.setColumnCount(self.collumn)
-        self.table.setHorizontalHeaderLabels(["Código", "Descrição", "Valor Unitário", "Tipo"])
-        self.table.setColumnWidth(0, 60)
-        self.table.setColumnWidth(1, 380)
-        self.table.setColumnWidth(2, 120)
-        for row_number, row_data in enumerate(self.result):
-            self.table.insertRow(row_number)
-            for collumn_number, data in enumerate(row_data):
-                
-                if collumn_number == 2:
-                    valor = (f"R$ {data:.2f}")
-                    self.item = QtWidgets.QTableWidgetItem(str(valor).replace(".", ","))
-                elif collumn_number == 0:
-                    valor = (f"{data:0>4}")
-                    self.item = QtWidgets.QTableWidgetItem(str(valor))
-                else:
-                    self.item = QtWidgets.QTableWidgetItem(str(data))
-                if collumn_number == 0 or collumn_number == 2:
-                    self.item.setTextAlignment(QtCore.Qt.AlignRight)
-                else:
-                    self.item.setTextAlignment(QtCore.Qt.AlignLeft)
-                self.table.setItem(row_number, collumn_number, self.item)
-
